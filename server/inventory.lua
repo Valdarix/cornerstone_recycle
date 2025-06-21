f1339dd30f17bdd403e5319ff26ac8c4e346d37e
Cornerstone <james@valdarixgames.com>
2025-06-21 18:41:53 -0400

    Update inventory.lua

--- server/inventory.lua
+++ server/inventory.lua
@@ -1,10 +1,10 @@
 -- Inventory functions are routed through the Community Bridge
-
+local Bridge = exports.community_bridge:Bridge()
 ---@param src number # player source
 ---@param item string # item name
 ---@param amount number # amount to take from player
 function removeItem(src, item, amount)
-  Inventory.RemoveItem(src, item, amount)
+  Bridge.Inventory.RemoveItem(src, item, amount)
 end
 exports('removeItem', removeItem)
 
@@ -12,7 +12,7 @@ exports('removeItem', removeItem)
 ---@param item string # item name
 ---@param amount number # amount to take from player
 function addItem(src, item, amount)
-  local success = Inventory.AddItem(src, item, amount)
+  local success = Bridge.Inventory.AddItem(src, item, amount)
   if not success then
     doNotifyServer(src, 5000, 'Inventory:', 'You cant carry that!', 'error')
   end
