e00b703c33ae32aa7669ea6f663f6a4be53d188d
Cornerstone <james@valdarixgames.com>
2025-06-21 18:40:53 -0400

    Update notify.lua

--- client/notify.lua
+++ client/notify.lua
@@ -1,9 +1,10 @@
+local Bridge = exports.community_bridge:Bridge()
 ---@param duration number 
 ---@param title string 
 ---@param desc string 
 ---@param type string 
 function doNotifyClient(duration, title, desc, type)
   DebugPrint('doNotifyClient via Community Bridge')
-  Notify.SendNotify(title .. ': ' .. desc, type, duration)
+  Bridge.Notify.SendNotify(title .. ': ' .. desc, type, duration)
 end
 exports('doNotifyClient', doNotifyClient)
